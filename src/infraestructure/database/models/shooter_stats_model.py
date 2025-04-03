c3fb81614f15e4883c54c7a9d71c7802d4198b7e
Angel Zorrilla <whoangel.agl@gmail.com>
2025-04-02 19:11:18 -0600

    Agregando mas datos a la tabla de shooter stats

--- src/infraestructure/database/models/shooter_stats_model.py
+++ src/infraestructure/database/models/shooter_stats_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, UUID, DateTime, func, ForeignKey, Integer, Float
+from sqlalchemy import Column, UUID, DateTime, func, ForeignKey, Integer, Float, String
 from sqlalchemy.orm import relationship
 from src.infraestructure.database.session import Base
 
@@ -9,12 +9,22 @@ class ShooterStatsModel(Base):
     total_shots = Column(Integer, default=0, nullable=False)
     accuracy = Column(Integer, default=0, nullable=False) # Porcentaje de precision
     # average_score = Column(Integer, default=0, nullable=False)
+    reaction_shots = Column(Integer, default=0, nullable=False)
+    presicion_shots = Column(Integer, default=0, nullable=False)
+    draw_time_avg = Column(Float, default=0, nullable=False)
+    reload_time_avg = Column(Float, default=0, nullable=False)
+
     average_hit_factor = Column(Float, default=0, nullable=False)
     effectiveness = Column(Float, default=0, nullable=False)
+
+    common_error_zones = Column(String, nullable=True)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
     shooter = relationship("ShooterModel", back_populates="stats")
+
+    def __repr__(self):
+        return f"ShooterStats(shooter_id={self.shooter_id})"
 
 from src.infraestructure.database.models.shooter_model import ShooterModel
