c9af55f32994259772896423ea6fa9ff84193240
Angel Zorrilla <whoangel.agl@gmail.com>
2025-07-09 19:17:02 -0600

    cambiando ok por successful

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/health")
 def health_check():
-    return {"status": "OK", "host": settings.HOST, "port": settings.PORT}
+    return {"status": "SUCCESSFULL", "host": settings.HOST, "port": settings.PORT}
 
 
 app.include_router(router_v1)
