68a69ad59dc9eb45792147c7413c64c145f6474e
Angel Zorrilla <whoangel.agl@gmail.com>
2025-07-09 22:26:17 -0600

    fix: successssss

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/health")
 def health_check():
-    return {"status": "OK lesgo!!!!", "host": settings.HOST, "port": settings.PORT}
+    return {"status": "SUCCESSSSSS", "host": settings.HOST, "port": settings.PORT}
 
 
 app.include_router(router_v1)
